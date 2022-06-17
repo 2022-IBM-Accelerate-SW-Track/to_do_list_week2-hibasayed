31d36af887d4cf926f22f1d4501038ed108cbe59
hibasayed <hibasayed@gmail.com>
2022-06-17 16:14:07 -0500

    Introduction added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profile_pic_name from "../assets/profile_pic_name.jpeg";
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src={profile_pic_name}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Hiba Sayed</div>
+        <div className="brief_description">
+          Hello! My name is Hiba Sayed and I am a sophomore computer science student at Virginia Tech with a minor in mathematics.
+          I am currently working at Deloitte this summer as a Cyber Discovery Intern in the Risk and Financial Advisory Group as well
+          as taking part in IBM's Accelerate Program completing the Software track. I am passionate in increasing diversity in STEM
+          through my role as Treasurer of my school's Society of Women Engineer's chapter as well as attending various conferneces
+          such as Grace Hopper and Tapia in 2021.    
         </div>
       </div>
-    );
+    </div>
+  </div>
+    )
   }
 }
